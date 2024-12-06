02e2fe47fa55459f39952c10adad1b72498f68fb
lee-dooghong <lovelyldh@naver.com>
2024-12-06 18:00:20 +0900

    1311

--- 1311.py
+++ 1311.py
@@ -8,30 +8,64 @@ D = [list(map(int, rl().split())) for _ in range(N)]
 
 # 전체 숫자의 갯수
 bit_num = int(math.pow(2, N)) - 1
+# print(f'bit_num : {bin(bit_num)}')
 
-# dp는 i는 누가 일을 한건지 m은 어떤일이 처리된건지
+# dp는 i는 누구까지 일이 처리된건지? j는 어떤일이 처리된건지
 # 마지막에는 dp[N - 1][bit_num]을 출력하면 해결된다.
-dp = [[0 for _ in range(bit_num)] for _ in range(N)] 
+dp = [[sys.maxsize for _ in range(bit_num + 1)] for _ in range(N)] 
+# for i in dp :
+#     print(i)
 
-print(bit_num)
+# print(bit_num)
+# print(dp[2][bit_num])
 
 # n 현재 처리해야할 사람(10진수), m 현재까지 처리된 일(2진수), cost 현재까지 비용(10진수)
+# bottom-up
 def dfs(n, m, cost) :
-    print(f'[dfs] n : {n}, m :{m}, cost : {cost}')
+    if (n == N or m == bit_num) : return
+    # if (dp[n][m] != 0) : return dp[n][m]
+    # print('[dfs] {1}번째 인간에 대해 탐색 중, 현재까지 처리된 일 : {0:0>3}, 현재까지 비용 : {2}'.format(bin(m)[2:], n+1, cost))
     for i in range(N) :
         #처리안한일이 있으면 
         if (m & int(math.pow(2, i))) == 0 :
+            # print('[dfs] {1}번째 인간에 대해 탐색 중, 현재까지 처리된 일 : {0:0>3}, 현재까지 비용 : {2}, {3:0>3}번째 일을 탐색'.format(bin(m)[2:], n+1, cost, bin(i)[2:]))
             # 현재까지의 값과 더해서 예상되는 값을 기존의 dp값과 비교해본다.
             # 현재까지 처리된 값
             work_progress = m | int(math.pow(2, i))
-            print(f'[dfs] work_progress : {bin(work_progress)}')
+            # print(f'[dfs] work_progress : {bin(work_progress)}, 10진수 {work_progress}')
 
-            if cost + D[n][i] < dp[n][work_progress] or dp[n][work_progress] == 0:
+            dp[n][work_progress] = math.min(dfs)
+            if cost + D[n][i] < dp[n][work_progress]:
                 dp[n][work_progress] = cost + D[n][i]
                 dfs(n+1, work_progress, dp[n][work_progress])
-    print("dfs")
+    
+
+#top-down 방식으로한다면..
+# def dfs_top_down(n, m) :
+#     if dp[n][m] != 0 : return dp[n][m]
+
+def dfs2(n) : # n은 몇번째 사람에 대해서 현재 처리중인지를 하는 것임
+    for i in range(bit_num) :
+        # print(i)
+        former_N = dp[n-1][i]
+        if n == 0 :
+            former_N = 0
+        else :
+            if former_N == sys.maxsize : continue
+
+        for j in range(N) : 
+            dp[n][i | j] = min(former_N + D[n][j], dp[n][i | j])
+            # print('dfs2[{0}] dp[{1}][{2}] = {3}'.format(n, n, bin(i|j), dp[n][i | j]))
 
 if __name__ == "__main__" :
-    print("main")
-    dfs(0, 0, 0)
+    for i in dp :
+        print(dp)
+
+    for i in range(N) :
+        dfs2(i)
+    
+    for i in dp :
+        print(dp)
+
     print(dp[N-1][bit_num])
+
--- test.py
+++ test.py
@@ -1,10 +1,9 @@
 from sys import maxsize
 from sys import stdin
 
-bit = 0b001100
-print (bin(bit | 0b11 | 0b110000))
-#*은 이미 받은 것을 자르는 개념이네...
-#map을 돌리고 나면 그건 그냥 map 객체일 뿐이지 그 이상 뭐가 되는 건 아니다
-#다시 list로 활용하려면 list()로 캐스팅 해줘야 ㅎ나다.
-#근데 또 숫자 배당은 된다. 이건 그냥 기능인듯..
-#파이썬은 그냥 알아서 해주는게 너무 많아서.... 당연히 이것도 되겠지 했다가 안되는게 너무 많음
+
+arr = [0, 0, 1]
+for a in arr : 
+    if a == 0 : a == 100
+
+print(arr)
