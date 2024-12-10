c8a19ee2212cd15ed2c50a9d89ac0c94815168ae
lee-dooghong <lovelyldh@naver.com>
2024-12-10 19:00:43 +0900

    1285

--- 1285.py
+++ 1285.py
@@ -1,37 +1,90 @@
 #T의 갯수를 최대한 줄이기
 #T를 0으로, H를 1으로..
 import sys
+import copy
+from itertools import combinations
 
 rl = sys.stdin.readline
 N = int(input())
+
 xor_N = (1 << N) - 1
 
-arr = [int(rl().strip().replace('H', '1').replace('T', '0'), 2) for _ in range(0, N)]
+arr_raw = [int(rl().strip().replace('H', '1').replace('T', '0'), 2) for _ in range(0, N)]
+arr_raw_rev = [i ^ xor_N for i in arr_raw]
+
+arr_num = [i for i in range(N)]
 arr_asmbl = []
 
-def dfs(n, prcs_arr) :
-    global arr_asmbl
-    print(f'f : {n}, prcs_arr : {prcs_arr}')
+min_N = sys.maxsize
+
+def dfs(n, processing_arr) :
     if n == N :
-        arr_asmbl.append(prcs_arr)
+        arr_asmbl.append(processing_arr)
         return
     
-    dfs(n+1, prcs_arr)
-    prcs_arr[n] = prcs_arr[n] ^ xor_N
-    dfs(n+1, prcs_arr)
+    # print(f'f : {n}, prcs_arr : {processing_arr}')
+    dfs(n+1, processing_arr)
+    
+    tmp_arr = copy.deepcopy(processing_arr)
+    tmp_arr[n] = processing_arr[n] ^ xor_N
+    dfs(n+1, tmp_arr)
 
-def to_bin_arr(a) :
-    # tmp_arr = []
+def make_comb(n) :
+    global arr_asmbl
+    tmp_arr = []
+    for i in combinations(arr_num, n) :
+        for j in range(N) :
+            if j in i :
+                tmp_arr.append(arr_raw[j])
+            else : 
+                tmp_arr.append(arr_raw_rev[j])
+        arr_asmbl.append(tmp_arr)
+        tmp_arr = []
+            
+def to_bin_arr(arr) :
+    tmp_arr = ""
     for i in range(len(arr)) :
-        a.append('{0:0>3}'.format(bin(a[i])[2:]))
-    return a
+        tmp_arr += "{0:0>3}".format(bin(arr[i])[2:]) + "\n"
+    return tmp_arr
+
+def row_cal(arr) :
+    # print(f'[row_cal] arr : {arr}')
+    global min_N
+
+    return_N = 0
+
+    for i in range(len(arr)) :
+        tmp_N = 1 << i
+        T_num_tmp = 0
+        H_num_tmp = 0    
+        for l in arr :
+            # print(f'tmpN : {tmp_N}')
+            # print(f'l[i] : {l}')
+            if(tmp_N & l) : H_num_tmp += 1
+            else : T_num_tmp += 1
+        return_N += min(T_num_tmp, H_num_tmp)
+    # print(f'[row_cal] return_N : {return_N}')
+    return return_N
 
 if __name__ == '__main__' :
-    print(f'arr(bin) : {to_bin_arr(arr)}')
-    print(f'arr : {arr}')
-    dfs(0, arr)
-    print(f'arr(after) : {arr}')
+    # print(f'arr(bin) : {to_bin_arr(arr_raw)}')
+    # print(f'arr : {arr_raw}')
+    # dfs(0, arr_raw)
+    # print(f'arr(after) : {arr_raw}')
+    
+
+    # print(min_N)
+
+    # print(to_bin_arr(arr_raw))
+    # print(to_bin_arr(arr_raw_rev))
+    # print(arr_num)
+    for i in range(N + 1) : 
+        make_comb(i)
+    # print(arr_asmbl)
+
     for i in arr_asmbl : 
-        print(i)
+        min_N = min(min_N, row_cal(i))
+    print(min_N)
+
 
 
--- test.py
+++ test.py
@@ -1,12 +1,46 @@
 from sys import maxsize
 from sys import stdin
 
-arr = [1]
+# arr = [1]
 
-def change_N(n) :
-    n.append(100)
+# def change_N(n) :
+#     n.append(100)
 
-if __name__ == '__main__' :
-    print(arr)
-    change_N(arr)
-    print(arr)
+# if __name__ == '__main__' :
+#     print(arr)
+#     change_N(arr)
+#     print(arr)
+
+# 함수안에서도 global을 선언하면 
+# 전역 변수를 바꿀수 있다.
+
+# def test_global() :
+#     global b 
+#     b = [1]
+
+# def test_global2() :
+#     return b + 100 
+
+# print(a)
+# print(id(a))
+# change_N2(a)
+# print(a)
+
+# 변수 그 자체를 바꾸는 거면 밖에도 영향을 안미치는데, list 객체 중 일부를 바꾸면 밖에 영향을 미치네?
+# print(a)
+# test_global2()
+# print(a)
+
+# print(b)
+# test_global2()
+# print(b)
+
+# print(b, id(b))
+# change_N2(b)
+# print(b)
+
+a = (1,2,)
+
+if 3 in a : print('yes')
+else : print('no')
+
