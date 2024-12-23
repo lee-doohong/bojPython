d461fb13c9dd88620cb4844a49c406259efeb773
lee-dooghong <lovelyldh@naver.com>
2024-12-23 17:38:09 +0900

    2099

--- test.py
+++ test.py
@@ -1,10 +1,6 @@
 from sys import maxsize
 from sys import stdin
 
-string1 = "122"
-string1 = string1.replace('1', '3')
-print(chr(65))
-print(string1)
 
-# if __name__ == '__main__' :
-#     DFS(1, [])
+for i in range(10, 1, -1) :
+    print(i)
