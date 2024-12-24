6e3df33a1fda158bc8b7d991d65c0b5a2fefac7a
lee-dooghong <lovelyldh@naver.com>
2024-12-24 22:55:54 +0900

    2099

---
+++ 2099.py
@@ -0,0 +1,73 @@
+import sys
+import copy
+
+rl = sys.stdin.readline
+arr_list = []
+
+def make_reverse(arr) :
+    tmp_arr = [[0 for _ in range(N)] for _ in range(N)]
+    for x in range(N) :
+        for y in range(N) :
+            tmp_arr[x][y] = arr[y][x]
+
+    return tmp_arr
+
+def multiply(arr1, arr2) :
+    for i in range(len(arr1)) :
+        if (arr1[i] * arr2[i]) == 1 : 
+            return 1
+    return 0
+
+def matrix_multiply(arr1_2d, arr2_2d) :
+    arr2_2d_r = make_reverse(arr2_2d)
+    empty_arr = [[0 for _ in range(N)] for _ in range(N)]
+    
+    for x in range(N) : # 실제 행렬 곱하기
+            for y in range(N) :
+                empty_arr[x][y] = multiply(arr1_2d[x], arr2_2d_r[y])
+    
+    return empty_arr
+
+def make_list(arr_target) :
+    tmp_arr = [a[:] for a in arr_target]
+    arr_list.append(tmp_arr)
+    for _ in range(20) :
+        tmp_arr = matrix_multiply(tmp_arr, tmp_arr)
+        arr_list.append(tmp_arr)
+
+if __name__ == "__main__" :
+    input_line = list(map(int, rl().split()))
+    N = input_line[0]; K = input_line[1]; M = input_line[2]
+
+    result = ""
+
+    arr = [[0 for _ in range(N)] for _ in range(N)] # 기본행렬
+
+    for i in range(N) :
+        tmp_arr = list(map(int, rl().split()))
+        arr[i][tmp_arr[0] - 1] = 1
+        arr[i][tmp_arr[1] - 1] = 1
+
+    make_list(arr)
+
+    K_list = list(bin(K)[2:])
+    K_list.reverse()
+
+    result_arr = [[0 for _ in range(N)] for _ in range(N)]
+    flag = True
+
+    for i in range(len(K_list)) :
+        if K_list[i] == "1" :
+            if flag :
+                result_arr = arr_list[i]
+                flag = False
+            else : result_arr = matrix_multiply(result_arr, arr_list[[i]])
+
+    for i in range(M) :
+        tmp_arr = list(map(int, rl().split()))
+        if result_arr[tmp_arr[0] - 1][tmp_arr[1] - 1] == 1 :
+            result += "death\n"
+        else :
+            result += "life\n"
+            
+    print(result.strip())
--- test.py
+++ test.py
@@ -1,6 +1,4 @@
 from sys import maxsize
 from sys import stdin
 
-
-for i in range(10, 1, -1) :
-    print(i)
+print(bin(1))
