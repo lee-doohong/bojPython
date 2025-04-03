350a7a2cb0135688a9aa47610bc6338199c9e1e3
lee-dooghong <lovelyldh@naver.com>
2025-04-04 07:02:11 +0900

    1994

--- 1994.py
+++ 1994.py
@@ -1,10 +1,15 @@
+import sys
+import math
+
 N =  int(input())
 
 arr = list(map(int, [input() for _ in range(N)]))
 arr.sort()
-
+# print(arr)
 #방문한 경우에는 True로 바꿔준다.
 visited = [[False for _ in range(N)] for _ in range(N)]
+
+final_cnt = 1
 
 # visited[i][j] : i에서 j로 가는걸 확인했다는 의미이다. - 여기서 i와 j는 첫번째와 그 다음 수가 
 #아니고 그냥 등차가 성립하는 모든 수
@@ -15,25 +20,54 @@ visited = [[False for _ in range(N)] for _ in range(N)]
 
 def solution() :
     global visited
+    global final_cnt
     
-    for i in range(N) : 
-        first_N = arr[i]
-        now_N
-        for j in range(i + 1, N) :
+    for i in range(N) :
+        # 첫번째 수는 first_N
+        first_index = i
+        second_index = 2001
+        cnt_tmp = 1
 
+        # print("for_1st, fist_index : {0}, second_index : {1}, cnt_tmp : {2}".format(first_index, second_index, cnt_tmp))
+
+        for j in range(i + 1, N) :
+            #방문한 적이 있다면 패스
             if(visited[i][j] == True) :
                 continue
             else : visited[i][j] = True
 
-            common_dif = arr[j] - first_N
-            
-            if (arr[j])
+            #등차의 기준이 되는 수 지정
+            second_index = j
+            #공차확인
+            common_dif = arr[second_index] - arr[first_index]
+            #카운트 하나 더 올려준다
+            cnt_tmp = 2
 
-    
+            # print("for_2st, fist_index : {0}, second_index : {1}, cnt_tmp : {2}, common_dif : {3}".format(first_index, second_index, cnt_tmp, common_dif))
+            
+            for k in range(j + 1, N) :
+                # 바로 직전 숫자와 등차가 같다면..  
+                if ((arr[k] - arr[second_index]) > common_dif) :
+                    break
+
+                if ((arr[k] - arr[second_index]) == common_dif) :
+                    if(visited[second_index][k] == True) :
+                        # 같은 차이가 나는 수를 확인했는데 이미 방문한 적이 있다면 굳이 더 할필요가 없으므로 break 한다.
+                        break
+                    else : visited[second_index][k] = True
+
+                    cnt_tmp += 1
+                    second_index = k
+
+                # print("for_3st, fist_index : {0}, second_index : {1}, k : {4} cnt_tmp : {2}, common_dif : {3}".format(first_index, second_index, cnt_tmp, common_dif, k))
+
+            final_cnt = max(final_cnt, cnt_tmp)
     return
 
 if __name__ == "__main__" : 
-    solution() 
+    solution()
+    # print("final_cnt : {0}".format(final_cnt))
+    print(final_cnt)
 
 
 
--- test.py
+++ test.py
@@ -1,14 +1,20 @@
 from sys import maxsize
 from sys import stdin
 
-<<<<<<< HEAD
-print(type(2**2))
-=======
 
-for i in range(1, 10) :
-    for j in range(1, 10) :
-        print(f'i : {i}, j : {j}')
-        if j == 5 : 
-            break
+test = [1, 3, 5, 10, 11, 13, 14]
 
->>>>>>> f6d3e3c9ee3cf652881d2191434778a483c0d6cd
+def bin_search (lo, hi, target) :
+    global test
+    while(lo < hi) : 
+        mid = int((lo + hi) / 2)
+        # 가운데 값보다 target값이 크거나 같으면 
+        if (test[mid] < target) :
+            lo = mid + 1
+        elif (test[mid] > target) :
+            hi = mid
+        print("lo : {0}, hi : {1}, mid : {3}, target : {2}".format(lo, hi, target, mid))
+    
+    return hi
+
+print(bin_search(0, len(test) - 1, 7))
