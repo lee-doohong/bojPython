416866cf7c1ebcd51e8fdfe5729a92d765f832fc
lee-dooghong <lovelyldh@naver.com>
2025-08-25 21:26:16 +0900

    ..

---
+++ 1068.py
@@ -0,0 +1,14 @@
+import sys
+
+class node :
+    def __init__(self, value) :
+        self.value = value
+        self.left = None
+        self.right = None
+
+def main():
+    N = int(input())
+
+    for  
+    
+    
--- test.py
+++ test.py
@@ -1,12 +1,4 @@
-from collections import defaultdict
+N = input()
+M = input()
 
-a = defaultdict(int)
-
-print(a)
-
-a['a'] = 1
-
-print(a['m'])
-
-딕셔너리에서 get은 keyError 발생을 방지하는 방법이다.
-1994 문제 풀이 해법은 diff 
+print(f"N = {N} M = {M}")
