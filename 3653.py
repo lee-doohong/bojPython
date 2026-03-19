04b7229089dc1c0486ea6994d22e27107866ad4c
leedoohong <lovelyldh@naver.com>
2026-03-19 13:57:36 +0900

    .

--- 3653.py
+++ 3653.py
@@ -5,34 +5,26 @@ def input_v() :
 
 def sub_solution(n, m, movies) : # arr은 보려고 하는 영화
     tree = [0] * (n + m) * 4
-    # n은 가지고 있는 영화의 수(1 ~ n 까지임) m은 보려고 하는 영화의 수 이다.
-    # arr에서 보려고 하는 영화 때문에 비워둔건 0 ~ m - 1 까지 영화는 m 부터 m + n - 1 까지
-    # 예를 들어 m이 2개 n 이 3개라면 arr에서 0 은 0부터 1까지 // 1은 2부터 4까지 
     arr = [0] * m + [1] * n
-    
-    # 딕셔너리로 넣자 특정 영화번호 : 위치 
-    index = {i : i + m - 1 for i in range(1, n + 1)}
 
+    index = {i : i + m - 1 for i in range(1, n + 1)}
     tmp_result = []
      
-    print(f"movies : {movies}")
-
     def build(node, start, end) : 
-        print(f"[build] node : {node}, start : {start}, end : {end}")
         if start == end :
             tree[node] = arr[start]
             return tree[node]
         
         mid = int((start + end) / 2)
         tree[node] = build(node * 2, start, mid) + build(node * 2 + 1, mid + 1, end)
+        return tree[node]
 
     def update(node, start, end, index, val) :
-        print(f"[update] node : {node}, start : {start}, end : {end}, index : {index}, val : {val}")
         if index < start or end < index : 
             return 
         
         if start == end == index : 
-            tree[node] = val
+            tree[node] += val
             return
         
         mid = int((start + end) / 2)
@@ -43,7 +35,6 @@ def sub_solution(n, m, movies) : # arr은 보려고 하는 영화
         return
             
     def query(node, start, end, left, right) :
-        print(f"[query] node : {node}, start : {start}, end : {end}, left : {left}, right : {right}")
         if right < start or end < left :
             return 0
 
@@ -53,13 +44,13 @@ def sub_solution(n, m, movies) : # arr은 보려고 하는 영화
         mid = int((start + end) / 2)
         return query(node * 2, start, mid, left, right) + query(node * 2 + 1, mid + 1, end, left, right)
 
+    build(1, 0, m + n - 1)
+
     for i in range(m) :
         tmp_result.append(str(query(1, 0, m + n - 1, 0, index[movies[i]] - 1)))
-        print(f"index : {index}")
         update(1, 0, m + n - 1, index[movies[i]], -1)
         update(1, 0, m + n - 1, m - (i + 1), 1)
         index[movies[i]] = m - (i + 1)
-        print(f"index : {index}")
    
     return " ".join(tmp_result)
 
